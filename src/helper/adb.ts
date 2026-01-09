6abd7fb7a35d0ab109261b04391b462996485215
Asaki Yuki <vantrong2007vn@gmail.com>
2026-01-09 15:52:22 +0700

    add wsa application manager

--- src/helper/adb.ts
+++ src/helper/adb.ts
@@ -1,6 +1,7 @@
 import { exec } from "child_process"
-import { isRunning, isRunningSync } from "./wsa"
+import { isRunning } from "./wsa"
 import { promisify } from "util"
+import { LocalStorage } from "@raycast/api"
 
 const execAsync = promisify(exec)
 
@@ -35,10 +36,64 @@ export async function connect(port: number = 58526) {
 	}
 }
 
-export async function listApplications() {
+export enum PackagesFilter {
+	ALL = "",
+	DISABLE = "-d",
+	ENABLE = "-e",
+	THIRD_PARTY = "-3",
+	SYSTEM = "-s",
+}
+
+export interface AppInfo {
+	Id: string
+	DisplayName?: string
+	DisplayIcon?: string
+}
+
+export async function getPackageRegistryInfos(packageId: string): Promise<AppInfo | null> {
+	if (/^(\w|\.)+$/.test(packageId)) {
+		try {
+			const value = await LocalStorage.getItem(`wsa-${packageId}`)
+			if (value && typeof value === "string") return JSON.parse(value)
+
+			const result = await execAsync(
+				"reg query HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\" + packageId
+			).then(res => res.stdout)
+
+			const info: AppInfo = {
+				Id: packageId,
+			}
+
+			result
+				.split("\n")
+				.slice(1)
+				.forEach(value => {
+					const [$1, $2, $3] = value.trim().split("    ")
+					if (["DisplayName", "DisplayIcon"].includes($1)) info[$1 as keyof typeof info] = $3
+				})
+
+			await LocalStorage.setItem(`wsa-${packageId}`, JSON.stringify(info))
+
+			return info
+		} catch (error) {
+			await LocalStorage.setItem(`wsa-${packageId}`, JSON.stringify({ Id: packageId }))
+			return {
+				Id: packageId,
+			}
+		}
+	} else return null
+}
+
+export async function listPackages(filter: PackagesFilter = PackagesFilter.ALL) {
 	try {
+		if (!(await isRunning())) return null
+
+		return (await Promise.all(
+			await execAsync("adb shell pm list packages " + filter, { encoding: "utf-8" }).then(res =>
+				(res.stdout.match(/(\w|\.)+$/gm) || []).map(getPackageRegistryInfos)
+			)
+		)) as AppInfo[]
 	} catch (error) {
-		console.error(error)
 		return null
 	}
 }
--- src/wsa-appications.tsx
+++ src/wsa-appications.tsx
@@ -1,14 +1,20 @@
-import { Detail, List, popToRoot, showToast, Toast } from "@raycast/api"
+import { Action, ActionPanel, Detail, List, showHUD, showToast, Toast } from "@raycast/api"
 import { useState } from "react"
-import { ConnectState, connect } from "./helper/adb"
-import { usePromise } from "@raycast/utils"
+import { PackagesFilter, ConnectState, connect, listPackages, AppInfo } from "./helper/adb"
+import { runPowerShellScript, usePromise } from "@raycast/utils"
 
 export default function Command() {
 	const [connected, setConnected] = useState<ConnectState>(ConnectState.CONNECTING)
+	const [appLoaded, setAppLoaded] = useState(false)
+	const [list, setList] = useState<Array<AppInfo>>([])
 	const [search, setSearch] = useState("")
 
 	usePromise(async () => {
+		showToast(Toast.Style.Animated, "Connecting to WSA...")
 		setConnected(await connect())
+		showToast(Toast.Style.Animated, "Loading applications...")
+		setList((await listPackages(PackagesFilter.ALL)) || [])
+		showToast(Toast.Style.Success, "Applications loaded")
 	})
 
 	return connected === ConnectState.CONNECTING ? (
@@ -24,6 +30,26 @@ export default function Command() {
 			navigationTitle="Windows Subsystem for Android"
 			searchBarPlaceholder="Search Applications..."
 			onSearchTextChange={setSearch}
-		></List>
+		>
+			{list.map(({ Id, DisplayIcon, DisplayName }) => (
+				<List.Item
+					key={Id}
+					title={DisplayName || Id}
+					subtitle={DisplayName ? Id : undefined}
+					icon={DisplayIcon}
+					actions={
+						<ActionPanel title="Windows Subsystem for Android">
+							<Action
+								title="Launch"
+								onAction={async () => {
+									await showHUD(`Launching ${DisplayName || Id}...`)
+									await runPowerShellScript(`WsaClient.exe /launch wsa://${Id}`)
+								}}
+							/>
+						</ActionPanel>
+					}
+				/>
+			))}
+		</List>
 	)
 }
